9a7a35ba3dabdfaaf176df8004f7d9ddd61abf0d
Olha Tkachiv <olyatkachiv@gmail.com>
2025-06-24 12:54:52 +0200

    Updated portfolio content

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Set the document title
+document.title = "Olha Tkachiv Portfolio"
+
+// Set meta description
+const metaDescription = document.querySelector('meta[name="description"]')
+if (metaDescription) {
+  metaDescription.setAttribute(
+    "content",
+    "Portfolio of Olha Tkachiv - Passionate Front-End Developer and Project Manager specializing in React, JavaScript, and responsive web design. Based in Copenhagen, Denmark.",
+  )
+} else {
+  const meta = document.createElement("meta")
+  meta.name = "description"
+  meta.content =
+    "Portfolio of Olha Tkachiv - Passionate Front-End Developer and Project Manager specializing in React, JavaScript, and responsive web design. Based in Copenhagen, Denmark."
+  document.getElementsByTagName("head")[0].appendChild(meta)
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </React.StrictMode>,
+)
